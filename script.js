41752a620df9775f2483bdf77e3b028096851d03
omer rokach <omerrokach@gmail.com>
2021-01-21 23:31:53 +0200

    building by hand objects

---
+++ script.js
@@ -0,0 +1,152 @@
+const task1 = {
+    startedAt : new Date("2021-01-20:08:00"),
+
+    finishedAt : new Date("2021-01-20:19:00"),
+
+    totalTime = (finishedAt.getHours()-startedAt.getHours()),
+
+    tasksGiven : 10,
+
+    tasksFinished : 7,
+
+    taskPercentage = Math.floor((this.tasksFinished/this.tasksGiven)*100),
+
+    topic : 'javaScript'
+    },
+const task2 = {
+    startedAt : new Date("2021-01-21:09:00"),
+    
+    finishedAt : new Date("2021-01-21:20:00"),
+    
+    totalTime = (finishedAt.getHours()-startedAt.getHours()),
+    
+    tasksGiven : 15,
+    
+    tasksFinished : 12,
+    
+    taskPercentage = Math.floor((this.tasksFinished/this.tasksGiven)*100),
+    
+    topic : 'CSS'
+    },
+const task3 = {
+    startedAt : new Date("2021-01-22:10:00"),
+    
+    finishedAt : new Date("2021-01-22:19:00"),
+    
+    totalTime = (finishedAt.getHours()-startedAt.getHours()),
+    
+    tasksGiven : 9,
+    
+    tasksFinished : 7,
+    
+    taskPercentage = Math.floor((this.tasksFinished/this.tasksGiven)*100),
+    
+    topic : 'javaScript'
+    },
+const task4 = {
+    startedAt : new Date("2021-01-23:08:00"),
+    
+    finishedAt : new Date("2021-01-23:20:00"),
+    
+    totalTime = (finishedAt.getHours()-startedAt.getHours()),
+    
+    tasksGiven : 10,
+    
+    tasksFinished : 5,
+    
+    taskPercentage = Math.floor((this.tasksFinished/this.tasksGiven)*100),
+    
+    topic : 'CSS'
+    },
+const task5 = {
+    startedAt : new Date("2021-01-24:11:00"),
+    
+    finishedAt : new Date("2021-01-24:19:00"),
+    
+    totalTime = (finishedAt.getHours()-startedAt.getHours()),
+    
+    tasksGiven : 20,
+    
+    tasksFinished : 15,
+    
+    taskPercentage = Math.floor((this.tasksFinished/this.tasksGiven)*100),
+    
+    topic : 'javaScript'
+    },
+const task6 = {
+    startedAt : new Date("2021-01-25:12:00"),
+    
+    finishedAt : new Date("2021-01-25:20:00"),
+    
+    totalTime = (finishedAt.getHours()-startedAt.getHours()),
+    
+    tasksGiven : 16,
+    
+    tasksFinished : 7,
+    
+    taskPercentage = Math.floor((this.tasksFinished/this.tasksGiven)*100),
+    
+    topic : 'CSS'
+    },
+const task7 = {
+    startedAt : new Date("2021-01-26:13:00"),
+    
+    finishedAt : new Date("2021-01-26:19:00"),
+    
+    totalTime = (finishedAt.getHours()-startedAt.getHours()),
+    
+    tasksGiven : 18,
+    
+    tasksFinished : 17,
+    
+    taskPercentage = Math.floor((this.tasksFinished/this.tasksGiven)*100),
+    
+    topic : 'CSS'
+    },
+const task8 = {
+    startedAt : new Date("2021-01-27:09:00"),
+    
+    finishedAt : new Date("2021-01-27:20:00"),
+    
+    totalTime = (finishedAt.getHours()-startedAt.getHours()),
+    
+    tasksGiven : 16,
+    
+    tasksFinished : 16,
+    
+    taskPercentage = Math.floor((this.tasksFinished/this.tasksGiven)*100),
+    
+    topic : 'javaScript'
+    },
+const task9 = {
+    startedAt : new Date("2021-01-28:09:00"),
+    
+    finishedAt : new Date("2021-01-28:19:00"),
+    
+    totalTime = (finishedAt.getHours()-startedAt.getHours()),
+    
+    tasksGiven : 8,
+    
+    tasksFinished : 7,
+    
+    taskPercentage = Math.floor((this.tasksFinished/this.tasksGiven)*100),
+    
+    topic : 'javaScript'
+    },
+const task10 = {
+    startedAt : new Date("2021-01-29:10:00"),
+    
+    finishedAt : new Date("2021-01-29:19:00"),
+    
+    totalTime = (finishedAt.getHours()-startedAt.getHours()),
+    
+    tasksGiven : 10,
+    
+    tasksFinished : 9,
+    
+    taskPercentage = Math.floor((this.tasksFinished/this.tasksGiven)*100),
+    
+    topic : 'javaScript'
+    }
+
+const arrTask = [task1, task2, task3, task4, task3, task4, task5, task6, task7, task8, task9, task10];
